42711a0dd6c5c8d9ab23e72af013a4ae1d278fdb
Cyberspace27 <ariel.villarreal26@gmail.com>
2016-08-10 18:34:30 -0600

    se modifica el datePicker pero todavia no se logra el funcionamiento optimo

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-angular.module('datePicker', ['$ui-bootstrap-tpls'])
-.controller('datePickerCtr', ['datePicker', '$ui-bootstrap',
-	function(datePicker){
+var angularDatePicker = angular.module('angularDatePicker', ['ui.bootstrap']);
 
-	}];
+angularDatePicker.controller("CtrlDatePicker",
+	function ($scope) {
+		
+		$scope.fechaActual = "10-08-2016";
+		$scope.listaPrueba = [
+		{nombre:"Ariel", fecha:"20-10-2016", edad:"28"}
+		];
+		
+
+		 $scope.clear = function() {
+    $scope.dt = null;
+	  };
+
+	  $scope.options = {
+	    customClass: getDayClass,
+	    minDate: new Date(),
+	    showWeeks: true
+	  };
+		
+	});
       	
